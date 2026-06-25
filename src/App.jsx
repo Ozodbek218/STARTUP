6d563a8a902d7fa69ab3fb395258ed304c151003
Ismingiz <emailingiz@gmail.com>
2026-06-25 14:26:08 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Pages from './Components/Pages'
+
+const App = () => {
+  return (
+    <div>
+      < Pages/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Pages.jsx
@@ -0,0 +1,13 @@
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
+
+const Pages = () => {
+  return (
+    <div>
+      < Header/>
+      < Footer/>
+    </div>
+  )
+}
+
+export default Pages
